86ede427650120f55f6971260115a81ad54ed058
juliemh <juliemh@optusnet.com.au>
2016-10-05 11:11:48 +1100

    Add files via upload

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -2,7 +2,7 @@
    
 
     <div class="box">
-<div class="header blue">
+<div class="head blue">
     <span>
     <i class="fa fa-tasks fa-5x"></i>
     <h3>Add Lecturer</h3>
@@ -21,7 +21,7 @@
     </div>
 
     <div class="box">
-<div class="header green">
+<div class="head green">
     <span>
     <i class="fa fa-plus-square fa-5x"></i>
     <h3>Add Course</h3>
@@ -37,7 +37,7 @@
         </div>
     </div>
     <div class="box">
-<div class="header yellow">
+<div class="head yellow">
     <span>
     <i class="fa fa-plus-circle fa-5x"></i>
     <h3>Add Subjects</h3>
@@ -54,9 +54,9 @@
     </div>
 
     <div class="box">
-        <div class="header orange">
+        <div class="head orange">
     <span>
-    <i class="fa fa-plus-upload fa-5x"></i>
+    <i class="fa fa-upload fa-5x"></i>
     <h3>Import Students</h3>
     </span>
     </div>
@@ -71,7 +71,7 @@
     </div>
 
     <div class="box">
-    <div class="header purple">
+    <div class="head purple">
     <span>
     <i class="fa fa-plus fa-5x"></i>
     <h3>Add Projects</h3>
@@ -88,7 +88,7 @@
     </div>
 
     <div class="box">
-    <div class="header blue">
+    <div class="head blue">
     <span>
     <i class="fa fa-plus-square-o fa-5x"></i>
     <h3>Add Skill</h3>
--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -12,17 +12,24 @@
 <title>Team Allocation Tool|Student |</title>
 
 <!-- Core CSS -->
-<link href="<?= base_url() ?>assets/css/tatui.css" rel="stylesheet">
+<link type="text/css" href="../assets/css/tatui.css" rel="stylesheet">
 
+<link type="text/css" href="../assets/css/bootstrap.css" rel="stylesheet">
 <!-- Custom Fonts -->
-<link href="<?= base_url() ?>assets/admin/css/font-awesome.min.css"
+<link href="../assets/css/font-awesome.min.css"
 	rel="stylesheet" type="text/css">
+
+<script src="../assets/js/jquery-2.1.3.min.js"></script>
+
+<script src="../assets/js/bootstrap.min.js"></script>
+
 
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
+
 
 </head>
 
@@ -31,14 +38,21 @@
 <div id="wrapper">
            <header>
             <div class="container"> 
-                <img class="rlogo" src="./assets/img/rmitlogo.png" alt="RMIT logo" />
+                <img class="rlogo" src="../assets/img/rmitlogo.png" alt="RMIT logo" />
                          
         
-                <img class="tlogo" src="./assets/img/tatlogo.png" alt="TAT logo" />
+                <img class="tlogo" src="../assets/img/tatlogo.png" alt="TAT logo" />
             </div>
   
-            <h2 class="welcome">Welcome <?php echo $firstname ?></h2></
-           </header>
+            <h2 class="welcome">Welcome <?php echo $this->session->userdata('admin_first');?></h2>
+          
+     
+		
+		
+         </header>
+    <div class="logout">
+                <a href="<?php echo base_url('login/admin_logout')?>" class="btn logoutbtn">Click me to Logout</a>
+     </div>
 		<!-- Navigation -->
                 <nav>
                     <div class="navContainer">
