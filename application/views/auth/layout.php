be72d720410b209c9484765f7ac910829a1e07fd
juliemh <juliemh@optusnet.com.au>
2016-10-05 11:12:29 +1100

    Delete layout.php

--- application/views/auth/layout.php
+++
@@ -1,4 +0,0 @@
-<?php
-$this->load->view($this->config->item('tatui_template_dir_auth') . 'header');
-$this->load->view($this->config->item('tatui_template_dir_auth') . 'content');
-$this->load->view($this->config->item('tatui_template_dir_auth') . 'footer');
