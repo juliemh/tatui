f8e34280655e4a938728d64c1c1216e2312e5c95
juliemh <juliemh@optusnet.com.au>
2016-10-09 20:05:17 +1100

    Delete layout.php

--- application/views/lecturer/layout.php
+++
@@ -1,4 +0,0 @@
-<?php
-$this->load->view($this->config->item('tatui_template_dir_lecturer') . 'header');
-$this->load->view($this->config->item('tatui_template_dir_lecturer') . 'content');
-$this->load->view($this->config->item('tatui_template_dir_lecturer') . 'footer');
