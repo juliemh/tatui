315907e609d3285871cd5a869a254cdb55f09c13
juliemh <juliemh@optusnet.com.au>
2016-10-09 19:58:09 +1100

    Delete layout.php

--- application/views/admin/layout.php
+++
@@ -1,4 +0,0 @@
-<?php
-$this->load->view($this->config->item('tatui_template_dir_admin') . 'header');
-$this->load->view($this->config->item('tatui_template_dir_admin') . 'content');
-$this->load->view($this->config->item('tatui_template_dir_admin') . 'footer');
