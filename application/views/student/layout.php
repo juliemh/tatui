dcccafa753c92f002a1db1cf27eb9872d08957ae
juliemh <juliemh@optusnet.com.au>
2016-10-09 20:05:56 +1100

    Delete layout.php

--- application/views/student/layout.php
+++
@@ -1,4 +0,0 @@
-<?php
-$this->load->view($this->config->item('tatui_template_dir_student') . 'header');
-$this->load->view($this->config->item('tatui_template_dir_student') . 'content');
-$this->load->view($this->config->item('tatui_template_dir_student') . 'footer');
