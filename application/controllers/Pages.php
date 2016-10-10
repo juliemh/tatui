b238207effaa94fcabf3152119314b4fbe2a887e
juliemh <juliemh@optusnet.com.au>
2016-10-11 08:04:13 +1100

    Update Pages.php

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -12,17 +12,16 @@ class Pages extends CI_Controller {
 	  {
 	      $data = array(
              'page_title' => 'Welcome!',
-            'title' => 'Login Page',
+             'title' => 'Login Page',
              'message' => 'Please login to access dashboard'
           );
-          $page = '/loginform';
-          
+          $page = 'loginform';
 		  $this->construct_pages($page, $data);
 	  } 
 	  // this is called to specify different pages
 	   public function construct_pages($page, $data) {	       
           $this->load->view('templates/header', $data);
-          $this->loadl->view('pages/'.$page);
-          $this->load->view('templates/footer');
+          $this->load->view('pages/'.$page);
+		  $this->load->view('templates/footer');
 	  }
 }
