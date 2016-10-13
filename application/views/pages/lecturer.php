ff47bfd52d1f75d632d5cacc4a8d7040e1b892bd
juliemh <juliemh@optusnet.com.au>
2016-10-14 08:09:19 +1100

    Update lecturer.php

--- application/views/pages/lecturer.php
+++ application/views/pages/lecturer.php
@@ -1,5 +1,77 @@
-<h2>Lecturer Dashboard</h2>
-<p>create projects</p>
-<p>add and remove students from a project</p>
-<p>add and remove skills from a project</p>
-<p>run algorithm</p>
+<div class="logout">
+                <a href="<?php echo base_url('logout')?>" class="btn logoutbtn">Click me to Logout</a>
+		
+		
+            </div>    	
+            </div>
+		<!-- Navigation -->
+                <nav>
+                    <div class="navContainer">
+                    <div class="dropdown">
+			<button class="dropbtn">Lecturer Menu</button>
+			<div class="dropdown-content">
+				<a href="#">Add Project</a> 
+                                <a href="#">Add Skill</a>
+                                <a href="#">Run Algorithm</a>
+			</div>
+                    </div>
+	</div>
+	<!-- end navigation -->
+	</nav>
+           <div id="page-wrapper">
+   
+ <div class="box">
+    <div class="head purple">
+    <span>
+    <i class="fa fa-plus fa-5x"></i>
+    <h3>Add Projects</h3>
+    </span>
+    </div>
+        <div class="bottom">
+            <span>
+            <a href="#">
+            <p>View details</p>
+            <i class="fa fa-arrow-circle-right"></i>
+            </a>
+            </span>
+        </div>
+    </div>
+
+    <div class="box">
+    <div class="head blue">
+    <span>
+    <i class="fa fa-plus-square-o fa-5x"></i>
+    <h3>Add Skill</h3>
+    </span>
+    </div>
+        <div class="bottom">
+            <span>
+            <a href="#">
+            <p>View details</p>
+            <i class="fa fa-arrow-circle-right"></i>
+            </a>
+            </span>
+        </div>
+    </div>
+   
+   
+    <div class="box">
+        <div class="head orange">
+    <span>
+    <i class="fa fa-play fa-5x"></i>
+    <h3>Run Algorithm</h3>
+    </span>
+    </div>
+        <div class="bottom">
+            <span>
+            <a href="#">
+            <p>View details</p>
+            <i class="fa fa-arrow-circle-right"></i>
+            </a>
+            </span>
+        </div>
+    </div>
+
+   
+</div>
+
