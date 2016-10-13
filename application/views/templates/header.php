e13137df89e77604b8525f331d5bf739bdd56cd5
juliemh <juliemh@optusnet.com.au>
2016-10-14 08:12:45 +1100

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,23 +1,53 @@
-<html>
-        <head>
-                <title><?php echo $page_title; ?></title>
-        </head>
-        <body>
+<!DOCTYPE html>
+<html lang="en">
 
-                <h1><?php echo $title; ?></h1>
-                <h2>
-                <?php 
-                
-                $this->load->helper(array('form','url'));
-                if ($this->session->userdata('user') == TRUE ) {
-                    echo "Welcome: ".$this->session->userdata('user'); 
-                    echo form_open('logout');
-                    echo form_submit('submit','logout');
-                    echo form_close();
-                      }
-                else 
-                    {
-                   
-                    }
-                ?>
-                </h2>
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+ <title><?php echo $page_title; ?></title>
+
+<!-- Core CSS -->
+<link type="text/css" href="<?php echo base_url() ?>assets/css/tatui.css" rel="stylesheet">
+
+<link type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.css" rel="stylesheet">
+<!-- Custom Fonts -->
+<link href="<?php echo base_url() ?>assets/css/font-awesome.min.css"
+	rel="stylesheet" type="text/css">
+
+<script src="<?php echo base_url() ?>assets/js/jquery-2.1.3.min.js"></script>
+
+<script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
+
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+</head>
+
+<body>
+
+	<div id="wrapper">
+           
+    
+        <header>
+            <div class="container"> 
+                <img class="rlogo" src="<?php echo base_url() ?>assets/img/rmitlogo.png" alt="RMIT logo" />
+                         
+        
+                <img class="tlogo" src="<?php echo base_url() ?>assets/img/tatlogo.png" alt="TAT logo" />
+            </div>
+  
+            <h2 class="welcome">Welcome </h2>
+           </header>
+          
+
+
+
