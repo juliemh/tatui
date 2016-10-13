29b7d1b7f66db6ebb56ef4c1cbde6c6976ddfadb
juliemh <juliemh@optusnet.com.au>
2016-10-14 08:07:51 +1100

    Update admin.php

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -1,6 +1,148 @@
-<h2>Admin Dashboard</h2>
-<p>create course</p>
-<p>set up user access</p>
-<p>import students</p>
-<p>set up projects</p>
-<p>manage skills dictionary</p>
+<div class="logout">
+                <a href="<?php echo base_url('logout')?>" class="btn logoutbtn">Click me to Logout</a>
+		
+		
+            </div>
+   		<!-- Navigation -->
+                <nav>
+                    <div class="navContainer">
+                    <div class="dropdown">
+			<button class="dropbtn">Admin Menu</button>
+			<div class="dropdown-content">
+				<a href="addlecturer">Add Lecturer</a> 
+                                <a href="addcourse">Add Course</a>
+                                <a href="addsubject">Add Subject</a>
+                                <a href="addproject">Add Project</a>
+                                <a href="addskill">Add Skill</a>
+                                <a href="#">Import Students</a>
+                                <a href="<?php echo base_url() ?>dashboard">Dashboard</a>
+			</div>
+
+                    </div>
+           </div>
+	</nav> 
+<div id="page-wrapper">
+
+    <div class="box">
+<div class="head blue">
+    
+    <span>
+    <i class="fa fa-tasks fa-5x"></i>
+    <h3>Add Lecturer</h3>
+    </span>
+    </div>
+        
+        <div class="bottom">
+            <span>
+                <a href="#">
+            <p>View details</p>
+            <i class="fa fa-arrow-circle-right"></i>
+            </a>
+            </span>
+        </div>
+            
+    </div>
+
+    <div class="box">
+<div class="head green">
+    <span>
+    <i class="fa fa-plus-square fa-5x"></i>
+    <h3>Add Course</h3>
+    </span>
+    </div>
+        <div class="bottom">
+            <span>
+            <a href="addcourse">
+            <p>View details</p>
+            <i class="fa fa-arrow-circle-right"></i>
+            </a>
+            </span>
+        </div>
+    </div>
+    <div class="box">
+<div class="head yellow">
+    <span>
+    <i class="fa fa-plus-circle fa-5x"></i>
+    <h3>Add Subjects</h3>
+    </span>
+    </div>
+        <div class="bottom">
+            <span>
+            <a href="addsubject">
+            <p>View details</p>
+            <i class="fa fa-arrow-circle-right"></i>
+            </a>
+            </span>
+        </div>
+    </div>
+
+    <div class="box">
+        <div class="head orange">
+    <span>
+    <i class="fa fa-upload fa-5x"></i>
+    <h3>Import Students</h3>
+    </span>
+    </div>
+        <div class="bottom">
+            <span>
+            <a href="#">
+            <p>View details</p>
+            <i class="fa fa-arrow-circle-right"></i>
+            </a>
+            </span>
+        </div>
+    </div>
+
+    <div class="box">
+    <div class="head purple">
+    <span>
+    <i class="fa fa-plus fa-5x"></i>
+    <h3>Add Projects</h3>
+    </span>
+    </div>
+        <div class="bottom">
+            <span>
+            <a href="addproject">
+            <p>View details</p>
+            <i class="fa fa-arrow-circle-right"></i>
+            </a>
+            </span>
+        </div>
+    </div>
+
+    <div class="box">
+    <div class="head blue">
+    <span>
+    <i class="fa fa-plus-square-o fa-5x"></i>
+    <h3>Add Skill</h3>
+    </span>
+    </div>
+        <div class="bottom">
+            <span>
+            <a href="#">
+            <p>View details</p>
+            <i class="fa fa-arrow-circle-right"></i>
+            </a>
+            </span>
+        </div>
+    </div>
+    <div class="box">
+   <div class="head blue">
+    <span>
+    <i class="fa fa-tasks fa-5x"></i>
+    <h3>Import Students</h3>
+    </span>
+    </div>
+        
+        <div class="bottom">
+            <span>
+            <a href="#"
+            <p>View details</p>
+            <i class="fa fa-arrow-circle-right"></i>
+            </a>
+            </span>
+        </div>
+            
+    </div>
+</div>
+
