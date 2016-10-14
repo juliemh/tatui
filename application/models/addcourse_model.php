01a0c15143241fb006e04a0e5b19892d8699efb7
juliemh <juliemh@optusnet.com.au>
2016-10-14 19:03:12 +1100

    Update addcourse_model.php

--- application/models/addcourse_model.php
+++ application/models/addcourse_model.php
@@ -2,24 +2,17 @@
 
 class Addcourse_model extends CI_Model {
 
-    /**
-    * Validate the login's data with the database
-    * @param string $email
-    * @param string $password
-    * @return void
-    */
-
-    /*Check Login*/
+    /*Check if the course exists*/
    	function validate($courseid)
 	{
              $query=$this->db->get_where('courses',array('CourseID'=>$courseid)); 
              return $query->result();	
 	}
 
-	/*Get Session values */
+	/* Add course  */
 		
 	function add_course($data){
-        // Inserting in Table(students) of Database(college)
+        // Insert into course table
         $this->db->insert('courses', $data);
           		
         }		
