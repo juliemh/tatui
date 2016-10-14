d16e9c4bbfd140ce8193c364c7334a6b45bd8dad
juliemh <juliemh@optusnet.com.au>
2016-10-14 14:45:12 +1100

    Update Addsubject.php

--- application/controllers/Addsubject.php
+++ application/controllers/Addsubject.php
@@ -1,4 +1,4 @@
-<?php if (! defined ( 'BASEPATH' )) exit ( 'No direct script access allowed' );
+<?php  defined ( 'BASEPATH' ) OR exit ( 'No direct script access allowed' );
 
 class Addsubject extends CI_Controller {
 
@@ -6,41 +6,48 @@ class Addsubject extends CI_Controller {
     public function __construct() {
 		parent::__construct ();
 		 $this->load->helper(array('form', 'url', 'html'));
+                 $this->load->helper('form');
                  $this->load->library('form_validation');
               //   $this->load->database();
 		 $this->load->model ( 'addsubject_model' );
                  
 	}
-	/*
-	 * Showing Login page here
-	 */
+	public function construct_pages($page, $data) {	       
+          $this->load->view('templates/header', $data);
+          $this->load->view('pages/'.$page);
+	  $this->load->view('templates/footer');
+	  }
+          
 	function index() {
-        
+        $display = 'addsubject';
+        $data = array(
+             'page_title' => 'Admin Add Subject',
+             'title' => 'Add Subjects'
+          );
             $data['courses'] = $this->addsubject_model->getCourses();
-            $this->load->view ( 'templates/header' );
-            $this->load->view ( 'pages/admin/navigation');
-            $this->load->view ( 'pages/admin/addsubject', $data  );
-            $this->load->view ('templates/footer');
+           $this->construct_pages($display, $data);
             $this->form_validation->set_rules('courses', 'Courses', 'callback_validate_dropdown');    
 	}
 
 	function validate() {
-            $message = "";
             $subjectid = $this->input->post ( 'subjectid' );
             $description = $this->input->post ( 'description' );
-            $courseid = $this->input->post ( 'courseid');
+            $courseid = $this->input->post ( 'courses');
+            echo $subjectid.' '.$description.' '.$courseid;
 
             $is_valid = $this->addsubject_model->validate ($subjectid, $courseid );
-		
-		if (!$is_valid)/*If not valid then the course doesn't exist */
+		echo 'validation';
+		if (!$is_valid)/*If not valid then the subject and course combination doesn't exist */
                 {
                     $data = array(
                     'SubjectID' => $subjectid,
                     'SubjectDesc' => $description,
                     'CourseID' => $courseid
                 );
-                    $this->session->set_flashdata('msg', "The subject was successfully added" );
-                   redirect( 'addsubject');
+                    $this->addsubject_model->add_subject ( $data );
+                    
+                   $this->session->set_flashdata ('msg', 'The subject '.$subjectid. ' was successfully added');
+               	 redirect( 'addsubject');
                   
                			}
                 else // course already exists 
