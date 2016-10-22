69ae18097f094d8e8b1626d6350e3f7470b39133
s3211063 <s3211063@student.rmit.edu.au>
2016-10-22 13:02:34 +1100

    Add files via upload

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Logout extends CI_Controller
 {
  function __construct()
@@ -9,21 +8,10 @@ class Logout extends CI_Controller
        
     }
     
-    public function construct_pages($page, $data) {	       
-          $this->load->view('templates/header', $data);
-          $this->load->view($page);
-		  $this->load->view('templates/footer');
-	  }
           
 public function index() {
    $this->session->unset_userdata('user');
    $this->session->sess_destroy();
-    $page = 'pages/loginform';
-		  $data = array(
-             'page_title' => 'Welcome!',
-             'title' => 'Login Page',
-            // 'message' => 'Please login to access dashboard',
-          );
-          $this->construct_pages($page, $data);
+   
    }
 }
