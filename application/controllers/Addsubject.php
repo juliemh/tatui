7f76ecf19e66ec39dbb275dc8337960ee5302ab4
s3211063 <s3211063@student.rmit.edu.au>
2016-10-26 07:03:44 +1100

    Add files via upload

--- application/controllers/Addsubject.php
+++ application/controllers/Addsubject.php
@@ -2,42 +2,46 @@
 
 class Addsubject extends CI_Controller {
 
-
     public function __construct() {
-		parent::__construct ();
-		 $this->load->helper(array('form', 'url', 'html'));
-                 $this->load->helper('form');
-                 $this->load->library('form_validation');
-              //   $this->load->database();
-		 $this->load->model ( 'addsubject_model' );
+		parent::__construct ();		 
+		 //This is the model for Adding a Project Task//
+		$this->load->helper(array('form', 'url', 'html'));
+		$this->load->library('form_validation');
+		//  
+		$this->load->model('add_subject_model');
                  
 	}
-	public function construct_pages($page, $data) {	       
-          $this->load->view('templates/header', $data);
-          $this->load->view('pages/'.$page);
-	  $this->load->view('templates/footer');
-	  }
+	public function call_page() {  
+        $usertype = 'admin';
+		// page data to be passed will be usertype by default
+		$data = array(
+			'page_title' => 'Welcome!',
+			'title' => $usertype,
+			'message' => '',
+			'includes' => 'pages/'.$usertype.'/add_subject'
+			   );
+        $this->load->view('templates/header', $data);
+        $this->load->view( 'pages/'.$usertype.'/nav');
+		$this->load->view('templates/content');
+		$this->load->view('templates/footer');
+		
+    }
           
 	function index() {
-        $display = 'addsubject';
-        $data = array(
-             'page_title' => 'Admin Add Subject',
-             'title' => 'Add Subjects'
-          );
-            $data['courses'] = $this->addsubject_model->getCourses();
-           $this->construct_pages($display, $data);
-            $this->form_validation->set_rules('courses', 'Courses', 'callback_validate_dropdown');    
+            $data['courses'] = $this->add_subject_model->getCourses();
+            $this->form_validation->set_rules('courses', 'Courses', 'callback_validate_dropdown'); 
+            $this->call_page();   
 	}
 
 	function validate() {
             $subjectid = $this->input->post ( 'subjectid' );
             $description = $this->input->post ( 'description' );
-            $courseid = $this->input->post ( 'courses');
+            $courseid = $this->input->post ( 'course');
             echo $subjectid.' '.$description.' '.$courseid;
 
-            $is_valid = $this->addsubject_model->validate ($subjectid, $courseid );
-		echo 'validation';
-		if (!$is_valid)/*If not valid then the subject and course combination doesn't exist */
+            $is_valid = $this->addsubject_model->validate ($subject_id, $course_id );
+		    echo 'validation';
+		    if (!$is_valid)/*If not valid then the subject and course combination doesn't exist */
                 {
                     $data = array(
                     'SubjectID' => $subjectid,
@@ -47,13 +51,13 @@ class Addsubject extends CI_Controller {
                     $this->addsubject_model->add_subject ( $data );
                     
                    $this->session->set_flashdata ('msg', 'The subject '.$subjectid. ' was successfully added');
-               	 redirect( 'addsubject');
+               	   redirect( 'addsubject');
                   
                			}
                 else // course already exists 
                 {	
                    $this->session->set_flashdata ( 'msg', 'This Subject and Course combination already exists' );
-			redirect ( 'addsubject' );
+			       redirect ( 'addsubject' );
 		}
 	}
 	
---
+++ application/controllers/Pages.php
@@ -0,0 +1,121 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pages extends CI_Controller { 
+
+function __construct() { 
+
+		parent::__construct(); 	
+						
+		$this->load->helper(array('form', 'url', 'html'));
+		$this->load->library('form_validation');
+		$this->load->model('get_user', '', TRUE);		
+	}	
+//	
+// calls login or page depending on context	
+//
+	 public function call_page($data, $usertype) {	 	 
+          $this->load->view('templates/header', $data);
+          if ($usertype != '') {
+              $this->load->view( 'pages/'.$usertype.'/nav');
+          }
+		  $this->load->view('templates/content');
+		  $this->load->view('templates/footer');		  
+    } 
+//
+// set session
+//       
+public function set_session($userid) {
+    $sess_data = array(
+			 'user' => $userid,
+			 'logged_in' => TRUE
+		 );
+		$this->session->set_userdata($sess_data);
+}
+//
+// get user access level from database which is student, lecturer and admin     
+//
+public function set_user($userid) {		
+		$this->set_session($userid);
+		 // get user level for page access
+		$usertype = $this->get_user->user_level($userid);
+		// get nav
+	
+		// page data to be passed will be usertype by default
+		$data = array(
+			'page_title' => 'Welcome!',
+			'title' => $userid,
+			'message' => '',
+			'includes' => 'pages/'.$usertype
+			   );
+		    $this->call_page($data, $usertype);  
+}
+//
+// exit validation only calls the login form
+// 
+public function exit_validate($message){
+       $usertype = '';
+       $data = array(
+			'page_title' => 'Welcome!',
+			'title' => 'Login Page',
+			'message' => $message,
+			'includes' => 'pages/loginform'
+			);
+		$this->call_page($data,$usertype);
+      }
+//      
+// initial validation of user import
+//      
+public function do_validation() {
+      // start of validation
+	      $userid = $this->input->post('userid');
+          $password = $this->input->post('password');
+          $this->form_validation->set_rules('userid', 'Userid', 'required|min_length[7]|max_length[7]');
+		  $this->form_validation->set_rules('password', 'Password', 'required|min_length[8]');		 		 
+		 // basic validation
+		  if ($this->form_validation->run() == false) {
+		            $message = "";
+		            $this->exit_validate($message);	
+		            return false;
+		      }
+	      // get user
+		   if ($this->get_user->get_user($userid) == false){		          
+		               $message = "user not found";
+                       $this->exit_validate($message);
+                       return false;
+           }
+		 //
+           if ( ($this->get_user->get_password($userid, $password)) == false ) {
+		               $message = "user password not found";
+		               $this->exit_validate($message);
+		               return false;
+                    }		
+           else 
+		    {	              
+		               $this->set_user($userid);
+		    } 
+
+}
+//
+// main routine that runs the form validation
+//
+public function index() {         
+          // see if user is set and take straight to their page
+         $user = $this->session->userdata('user');
+         if (isset($user)) {                     
+             $usertype = $this->get_user->user_level($user);
+                  $data = array(
+                        'page_title' => 'Welcome!',
+                        'title' => $usertype,
+                        'message' => '',
+                        'includes' => 'pages/'.$usertype
+                       );
+		        $this->call_page($data, $usertype);
+         }
+         else {
+         // if there is no session data then do validation
+             $message = '';
+             $this->do_validation();
+         }         
+     }   
+}
