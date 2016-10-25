57c1244a5231962a75819e402764c2303f2ab5ee
s3211063 <s3211063@student.rmit.edu.au>
2016-10-26 07:04:23 +1100

    Add files via upload

---
+++ application/views/pages/admin/nav.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+	   echo anchor('addproject', 'Add Project', 'title="Add Project"');
+	   echo "<br>";
+	   echo anchor('addsubject', 'Add Subject', 'title="Add Subject"');
+	   echo "<br>";
+        echo anchor('importusers', 'Import Users', 'title="Import Users"');
+	   echo "<br>";
+	   echo anchor('addskill', 'Add Skill', 'title="Add Skill"');
