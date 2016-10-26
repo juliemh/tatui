6796e4d6ed0b417eff25074b56b0322bb33a796c
juliemh <juliemh@optusnet.com.au>
2016-10-26 15:27:03 +1100

    Update addcourse.php

--- application/views/pages/admin/addcourse.php
+++ application/views/pages/admin/addcourse.php
@@ -23,6 +23,10 @@
 
                 <button name="submit" class="btn logoutbtn">  Submit  </button>
             </div>
+          <div class='login-username-container'>
+                 <a href="admin" class="btn logoutbtn">Back to Dashboard</a>
+            </div>
+        </div>
            
     </form>
 
