14ea549ba45a0c867abd8629b9ca99baf7a94794
s3211063 <s3211063@student.rmit.edu.au>
2016-10-31 21:00:02 +1100

    added name as salutation

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,63 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
+<head>
 
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
 
-        <title><?php echo $page_title; ?></title>
+ <title><?php echo $page_title; ?></title>
 
-        <!-- Core CSS -->
-        <link type="text/css" href="<?php echo base_url() ?>assets/css/tatui.css" rel="stylesheet">
-
-        <link type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.css" rel="stylesheet">
-        <!-- Custom Fonts -->
-        <link href="<?php echo base_url() ?>assets/css/font-awesome.min.css"
-              rel="stylesheet" type="text/css">
-
-        <script src="<?php echo base_url() ?>assets/js/jquery-2.1.3.min.js"></script>
-
-        <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
+<!-- Core CSS -->
+<link type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.css" rel="stylesheet">
+<link type="text/css" href="<?php echo base_url() ?>assets/css/tatui.css" rel="stylesheet">
 
 
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]>
-                    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-                    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-                <![endif]-->
+<!-- Custom Fonts -->
+<link href="<?php echo base_url() ?>assets/css/font-awesome.min.css"
+	rel="stylesheet" type="text/css">
 
-    </head>
+<script src="<?php echo base_url() ?>assets/js/jquery-3.1.1.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
 
-    <body>
-
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+</head>
+<body>
         <header>
-            <div class="container"> 
-                <img class="rlogo" src="<?php echo base_url() ?>assets/img/rmitlogo.png" alt="RMIT logo" />
-
-
-                <img class="tlogo" src="<?php echo base_url() ?>assets/img/tatlogo.png" alt="TAT logo" />
-            </div>
-            <?php $this->load->helper(array('form', 'url'));
-            if ($this->session->userdata('user') == TRUE) {
-                ?>
-                <h2 class="welcome">Welcome <?php echo $this->session->userdata('user'); ?></h2>
-                <?php
-            } else {
-                ?>
-                <h2 class="welcome">Welcome to The Team Allocation Tool</h2>
-                <?php
-            }
+          <img class="rmitlogo" src="<?php echo base_url() ?>assets/img/rmitlogo.png" alt="RMIT logo" />     
+          <img class="tatalogo" src="<?php echo base_url() ?>assets/img/tatlogo.png" alt="TAT logo" />
+          <div class="logout-button">
+           <?php 
+           $this->load->helper(array('form','url'));
+            if ($this->session->userdata('user') == TRUE ) {
+                     echo '<h3>Welcome '.$this->session->userdata('firstname').anchor('logout', '  Logout', 'title="Logout"').'</h3>';
+                }
+                else {
+                     echo '<h3>Welcome</h3>';
+               }
             ?>
-
-            
-        </header>
-        <section class="main-content">
-
-
+            </div>
+           </header>
+          <section class="main-content">
+          
 
 
 
