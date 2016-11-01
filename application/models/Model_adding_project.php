067ece599b797a174a7ae38f60b0520f5b6c217c
juliemh <juliemh@optusnet.com.au>
2016-11-01 22:25:35 +1100

    Update Model_adding_project.php

--- application/models/Model_adding_project.php
+++ application/models/Model_adding_project.php
@@ -13,16 +13,17 @@ class Model_adding_project extends CI_Model {
             foreach ($result->result_array() as $row) {
                 $drop[$row['course_id']] = $row['course_id'];
             }
+            $finalDropDown = array_merge(array('' => 'Please Select'), $drop);
         }
 
-        return $drop;
+        return $finalDropDown;
+        // return $drop;
     }
 
-    function getSubjectsByCourse($course = string) {
+   function getSubjectsByCourse($course = string) {
         $this->db->select('subject_id');
         $this->db->where('course_id', $course);
         $query = $this->db->get('course_subject');
-
         return $query;
     }
 
@@ -36,29 +37,84 @@ class Model_adding_project extends CI_Model {
     }
 
     //To get all Skills//
-    function getAllSkills() {
+    function getSkills() {
 
-        $query = $this->db->query('SELECT * FROM skill_dictionary');
-        $rows = $query->result();
-        $dataone = array('skills' => $rows);
-        return $query->result();
+        $this->db->select('skill_id');
+        $this->db->from('skill_dictionary');
+        $query = $this->db->get();
+
+        if ($query->result_array()) {
+            return $query->result_array();
+        } else {
+            return FALSE;
+        }
     }
 
     function add_project($data) {
 
-        $this->db->insert('projects', $data);
+        $this->db->insert('project', $data);
+        return TRUE;
     }
 
     function add_project_skills($data) {
-        $this->db->insert('project_skills', $data);
+
+
+        if (!$this->db->insert('project_skills', $data)) {
+         
+            return FALSE;
+        } else {
+            return TRUE;
+        }
     }
 
     //Check if the database is empty
     function chkempty($table) {
         $this->db->select('COUNT(*)');
         $query = $this->db->get($table);
+        $result = $query->row_array();
+        $count = $result['COUNT(*)'];
+        if ($count > 0) {
 
-        if ($query->result()) {
+            return TRUE;
+        } else {
+
+            return FALSE;
+        }
+    }
+
+    function validateProj($projectid) {
+        $this->db->select('project_id');
+        $this->db->where('project_id', $projectid);
+        $query = $this->db->get('project');
+
+        if ($query->result_array()) {
+
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    function validateProjCourse($projectid, $courseid) {
+        $this->db->select('project_id, course_id');
+        $this->db->where(array('project_id' => $projectid, ' course_id' => $courseid));
+        $query = $this->db->get('project');
+
+        if ($query->result_array()) {
+
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    function validate($projectid, $courseid, $subjectid) {
+        $this->db->select('project_id, course_id, subject_id');
+        $this->db->where(array('project_id' => $projectid, ' course_id' => $courseid, 'subject_id' => $subjectid));
+        $query = $this->db->get('project');
+
+        if ($query->result_array()) {
+
             return TRUE;
         } else {
             return FALSE;
