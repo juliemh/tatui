bd14c32f03e6aaa64c179f6fd0a2d9f903acb600
juliemh <juliemh@optusnet.com.au>
2016-11-01 22:24:01 +1100

    Update Addproject.php

--- application/controllers/Addproject.php
+++ application/controllers/Addproject.php
@@ -4,9 +4,8 @@ class Addproject extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->helper(array('form', 'url', 'html'));
+        $this->load->helper(array('form', 'url', 'html', 'inflector', 'date'));
         $this->load->library('form_validation');
-//   $this->load->database();
         $this->load->model('Model_adding_project');
     }
 
@@ -23,7 +22,7 @@ class Addproject extends CI_Controller {
         $usertype = 'admin';
         if ($this->Model_adding_project->chkempty('course') &&
                 $this->Model_adding_project->chkempty('subject') &&
-                $this->Model_adding_project->chkempty('skill_dictionary')) {
+                $this->Model_adding_project->chkempty('skill_dictionary') && $this->Model_adding_project->chkempty('semester')) {
             // page data to be passed will be usertype by default
             $data = array(
                 'page_title' => 'Add Project!',
@@ -31,20 +30,20 @@ class Addproject extends CI_Controller {
                 'message' => '',
                 'includes' => 'pages/' . $usertype . '/adding_Project'
             );
-
             $data['course'] = $this->Model_adding_project->getCourses();
-            $data['skills'] = $this->Model_adding_project->getAllSkills();
+            $data['skills'] = $this->Model_adding_project->getSkills();
+            $data['semester'] = $this->Model_adding_project->getSemester();
 
-            $this->form_validation->set_rules('project_id', 'Project ID', 'required');
-            $this->form_validation->set_rules('project_name', 'Project Name', 'required');
-            $this->form_validation->set_rules('project_desc', 'Project Description', 'required');
-            $this->form_validation->set_rules('start_date', 'Project Start', 'required');
-            $this->form_validation->set_rules('finish_date', 'Project Finish', 'required');
-            $this->form_validation->set_rules('team_size', 'Project Team', 'required');
-            $this->form_validation->set_rules('courses', 'Courses', 'callback_validate_dropdown');
-            $this->form_validation->set_rules('subjects', 'Subjects', 'callback_validate_dropdown');
-            $this->form_validation->set_rules('lecturers', 'Lecturerss', 'callback_validate_dropdown');
-            $this->form_validation->set_rules('skills', 'Skills', 'callback_validate_checkbox');
+//            $this->form_validation->set_rules('project_id', 'Project ID', 'required');
+//            $this->form_validation->set_rules('project_name', 'Project Name', 'required');
+//            $this->form_validation->set_rules('project_desc', 'Project Description', 'required');
+//            $this->form_validation->set_rules('start_date', 'Project Start', 'required');
+//            $this->form_validation->set_rules('finish_date', 'Project Finish', 'required');
+//            $this->form_validation->set_rules('team_size', 'Project Team', 'required');
+//            $this->form_validation->set_rules('courses', 'Courses', 'callback_validate_dropdown');
+//            $this->form_validation->set_rules('subjects', 'Subjects', 'callback_validate_dropdown');
+//            $this->form_validation->set_rules('skills', 'Skills', 'callback_validate_radio');
+//            $this->form_validation->set_rules('teamsize', 'Teamsize', 'required');
             $this->call_page($data);
         } else {
             $data = array(
@@ -52,7 +51,8 @@ class Addproject extends CI_Controller {
                 'title' => $usertype,
                 'message' => '',
                 'includes' => 'pages/' . $usertype . '/addproject_error'
-            );;
+            );
+            $this->call_page($data);
         }
     }
 
@@ -60,32 +60,65 @@ class Addproject extends CI_Controller {
         //Variables for Adding a Project//  
 
         $projectid = strtoupper($this->input->post('project_id'));
-        $projectname = humanize( $this->input->post('project_name'));
-        $description = humanize( $this->input->post('description') );
-        $subjectid = $this->input->post('subjectid');
-        $courseid = $this->input->post('courseid');
-        $startDate = $this->input->post('startDate');
-        $endDate = $this->input->post('endDate');
+        $projectname = humanize($this->input->post('project_name'));
+        $description = humanize($this->input->post('description'));
+        $subjectid = $this->input->post('subject');
+        $courseid = $this->input->post('course');
+        //  $semester = $this->input->post('semester');
+        $startDate = @date('Y-m-d', @strtotime($this->input->post('startdate')));
 
-        $is_valid = $this->Model_adding_project->validate($projectid, $courseid, $subjectid);
+        $finishDate = @date('Y-m-d', @strtotime($this->input->post('finishdate')));
+        $teamsize = $this->input->post('team_size');
+        $this->session->set_flashdata('msg', 'Start is ' . $startDate . ' Finish date is ' . $finishDate);
 
-        if (!$is_valid)/* If not valid then the subject and course combination doesn't exist */ {
-            $data = array(
-                'project_id' => $projectid,
-                'project_name' => $projectname,
-                'project_description' => $description,
-                'courses_id' => $courseid,
-                'subject_id' => $subjectid,
-                'startDate' => $startDate,
-                'endDate' => $endDate
-            );
-            $this->Model_adding_project->add_project($data);
 
-            $this->session->set_flashdata('msg', 'The project ' . $projectid . ' was successfully added');
+        if ($this->Model_adding_project->validate($projectid, $courseid, $subjectid)) {
+            $this->session->set_flashdata('msg1', 'This Project, ' . projectid . ' and Course ' . $courseid . ' and Subject ' . $subjectid . ' combination already exists');
             redirect('addproject');
-        } else { // course already exists 
-            $this->session->set_flashdata('msg', 'This Project, Course and Subject combination already exists');
-            redirect('addproject');
+        } else {
+            if ($this->Model_adding_project->validateProjCourse($projectid, $courseid)) {
+
+                $this->session->set_flashdata('msg1', 'This Project, ' . projectid . ' and Subject ' . $subjectid . ' combination already exists');
+                redirect('addproject');
+            } else {
+                if ($this->Model_adding_project->validateProj($projectid)) {
+                    $this->session->set_flashdata('msg1', 'This Project, ' . projectid . '  already exists');
+                    redirect('addproject');
+                } else {
+                    $data = array(
+                        'project_id' => $projectid,
+                        'project_name' => $projectname,
+                        'project_desc' => $description,
+                        'course_id' => $courseid,
+                        'subject_id' => $subjectid,
+                        'start_date' => $startDate,
+                        'finish_date' => $finishDate,
+                        'team_size' => $teamsize
+                    );
+
+                    if ($this->Model_adding_project->add_project($data)) {
+
+                        if (!empty($this->input->post('skillcheck'))) {
+                            foreach ($this->input->post('skillcheck') as $value) {
+                                $skilldata = array(
+                                    'project_id' => $projectid,
+                                    'skill_id' => $value,
+                                    'skill_level' => $this->input->post($value)
+                                );
+                                if (!$this->Model_adding_project->add_project_skills($skilldata)) {
+                                    $this->session->set_flashdata('msg1', 'Unable to add the skills');
+                                    redirect('addproject');                                    
+                                }
+                            }
+                        }
+                        $this->session->set_flashdata('msg', 'The project ' . $projectid . ' was successfully added');
+                        redirect('addproject');
+                    } else {
+                        $this->session->set_flashdata('msg1', 'Unable to add the project ' . $projectid);
+                        redirect('addproject');
+                    }
+                }
+            }
         }
     }
 
@@ -95,7 +128,7 @@ class Addproject extends CI_Controller {
         $courseid = $this->input->post('course', TRUE);
         echo $courseid;
 
-        //run the query for the cities we specified earlier
+
         $subjectData['subjects'] = $this->Model_adding_project->getSubjectsByCourse($courseid);
 
         $output = null;
@@ -106,20 +139,6 @@ class Addproject extends CI_Controller {
         }
 
         echo $output;
-    }
-
-    function updateSelected() {
-        $selected = $this->input->post('selected');
-        $projectid = $this->input->post('projectid');
-
-        foreach ($selected as $sid) {
-            $level = $sid->name;
-            $data = array(
-                'project_id' => $projectid,
-                'skill_id' => $sid['Skill_id'],
-                'skill_level' => $level);
-            $this->Model_adding_project->add_project_skill($data);
-        }
     }
 
 }
