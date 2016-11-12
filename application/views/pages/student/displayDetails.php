905ffde56296436072f2d8b9ab08649afc1162b1
juliemh <juliemh@optusnet.com.au>
2016-11-13 08:35:04 +1100

    add student pages

---
+++ application/views/pages/student/displayDetails.php
@@ -0,0 +1,35 @@
+  <h2>View your details here...</h2>
+<?php
+
+foreach ($details as $row)
+{
+    
+
+?>
+
+<h6>Your Name is  <?php echo $row['firstname']." ".$row['lastname']; ?>
+<h6>Your RMIT email address is <?php echo $this->session->userdata('user'); ?></h6>
+<h6>Your Gender is  <?php echo $row['gender']; ?></h6>
+<h6>The Course you are enrolled in is  <?php echo $row['course_id']." ".$row['course_description']; ?></h6>
+<?php } ?>
+
+
+  <h3>Subject details </h3>
+<table class="skilltable">
+    
+    <tr>
+        <th>Subject ID</th><th>Subject Name</th><th>Semester</th><th>Mark</th>
+    </tr>
+    <?php foreach($subjects as $row)
+    {
+       ?>
+    <tr>
+        <td><?php echo $row['subject_id'] ?></td>
+        <td><?php echo $row['subject_name'] ?></td>
+         <td><?php echo $row['semester_id'] ?></td>
+          <td><?php echo $row['subject_mark'] ?></td>
+    </tr>
+  <?php  } ?>
+</table>
+
+
---
+++ application/views/pages/student/manageskills.php
@@ -0,0 +1,46 @@
+  <h2>Manage your skills here...</h2>
+<div class="formcontainer">
+   
+    <form id="skillselect" method="post" action="./manageskills/validate">
+        <table>
+            <tr>
+                <th></th> <th>Skill Name</th><th>Beginner</th><th>Medium Level</th><th>Expert</th><th>Delete Skill</th>
+            </tr>
+            <?php foreach ($skills as $row) {
+                ?>
+
+                <tr>
+                    <td><input type="checkbox" name="skillcheck[]" <?php if (($row['skilllevel'] == 'beginner') || ($row['skilllevel'] == 'medium') || ($row['skilllevel'] == 'expert')) { ?> checked=checked <?php } ?>id="<?php echo $row['skill_id']; ?>" value="<?php echo $row['skill_id']; ?>" /></td>
+                    <td><label for="<?php echo $row['skill_id']; ?>"><?php echo $row['skill_id']; ?></label></td>
+                    <td><input type="radio" name="<?php echo $row['skill_id']; ?>" value="beginner" <?php if ($row['skilllevel'] == 'beginner') { ?> checked=checked <?php } ?> id="<?php echo $row['skill_id']; ?>"  /></td>
+                    <td><input type="radio" name="<?php echo $row['skill_id']; ?>" value="medium" <?php if ($row['skilllevel'] == 'medium') { ?> checked=checked <?php } ?>  id="<?php echo $row['skill_id']; ?>"   /></td>
+                    <td><input type="radio" name="<?php echo $row['skill_id']; ?>" value="expert" <?php if ($row['skilllevel'] == 'expert') { ?> checked=checked <?php } ?> id="<?php echo $row['skill_id']; ?>"   /></td>
+                    <td><input type="radio" name="<?php echo $row['skill_id']; ?>" id="<?php echo $row['skill_id']; ?>" value="delete"  /></td>
+                </tr>
+            <?php }
+            ?>
+        </table>
+
+        <div class='login-username-container'>
+            <a href="pages" class="btn logoutbtn">Back to Dashboard</a>
+            <input type="submit" value="Update" name="submit" class="btn logoutbtn"  />        
+        </div>
+    </form>
+
+</div>  
+
+
+<script type="text/javascript">
+
+    $('input:radio').click(function () {
+        value = $(this).attr('name');
+
+        var chk = $('input:checkbox[value="' + value + '"]').prop('checked', true);
+        if (!chk)
+        {
+            $('input:checkbox[value="' + value + '"]').prop('checked', true);
+        }
+
+    });
+
+</script>
