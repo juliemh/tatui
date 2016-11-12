c9bd048177c6a6dfc9ff428d7fa51a00304eb6ac
juliemh <juliemh@optusnet.com.au>
2016-11-13 08:36:23 +1100

    Update nav.php

--- application/views/pages/student/nav.php
+++ application/views/pages/student/nav.php
@@ -7,9 +7,9 @@
         <div class="dropdown">
             <button class="dropbtn">Student Menu</button>
             <div class="dropdown-content">
-                <a href="#">Display My Details</a>
-                <a href="#">Manage Skills</a> 
-                <a href="#">Register Projects</a>
+                <a href="displaydetails">Display My Details</a>
+                <a href="manageskills">Manage Skills</a> 
+                <a href="registerprojects">Register Projects</a>
             </div>
         </div>
     </div>
