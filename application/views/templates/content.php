42fb64ea7c348e43524f1a15043394d253256a1a
juliemh <juliemh@optusnet.com.au>
2016-11-13 08:44:51 +1100

    updates from John

--- application/views/templates/content.php
+++ application/views/templates/content.php
@@ -1,5 +1,8 @@
+<div id="page-wrapper">
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 // loads the content based on the page
 $this->load->view($includes);
+?>
+</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -11,25 +11,19 @@
 
  <title><?php echo $page_title; ?></title>
 
-<!-- Core CSS -->
-!-- Core CSS -->
- <link type="text/css" href="<?php echo base_url() ?>assets/css/tatui.css" rel="stylesheet">
-        <link type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap-datepicker3.min.css" rel="stylesheet">
-
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Droid+Serif" />
         <link type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.css" rel="stylesheet">
+        <link type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap-datepicker3.min.css" rel="stylesheet">
+        <link href="<?php echo base_url() ?>assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link type="text/css" href="<?php echo base_url() ?>assets/css/tatui.css" rel="stylesheet">
+
         <!-- Custom Fonts -->
-        <link href="<?php echo base_url() ?>assets/css/font-awesome.min.css"
-              rel="stylesheet" type="text/css">
-
         <script src="<?php echo base_url() ?>assets/js/jquery-2.1.3.min.js"></script>
-
         <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
         <script src="<?php echo base_url() ?>assets/js/bootstrap-datepicker.min.js"></script>
         <script src="<?php echo base_url() ?>assets/js/locales/bootstrap-datepicker.en-AU.min.js"></script>
-
-
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!--[if lt IE 9]>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
             <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
@@ -42,15 +36,15 @@
            <?php 
            $this->load->helper(array('form','url'));
             if ($this->session->userdata('user') == TRUE ) {
-                     echo '<h3>Welcome '.$this->session->userdata('firstname').anchor('logout', '  Logout', 'title="Logout"').'</h3>';
+                echo '<h1>Welcome: '.$this->session->userdata('firstname').anchor('logout', 'Logout', 'title="Logout" class="btn logoutbtn"').'</h1>';
                 }
                 else {
-                     echo '<h3>Welcome</h3>';
+                     echo '<h1>Welcome</h1>';
                }
             ?>
             </div>
            </header>
-          <section class="main-content">
+           <section class="main-content">
           
 
 
