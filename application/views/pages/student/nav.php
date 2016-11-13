6345b6e3448e6937ba7d5ccc33989de0d36c1c50
juliemh <juliemh@optusnet.com.au>
2016-11-13 17:13:36 +1100

    Add files via upload

--- application/views/pages/student/nav.php
+++ application/views/pages/student/nav.php
@@ -4,9 +4,11 @@
         <div class="dropdown">
             <button class="dropbtn">Student Menu</button>
             <div class="dropdown-content">
+                <a href="pages">Dashboard</a>
                 <a href="displaydetails">Display My Details</a>
                 <a href="manageskills">Manage Skills</a> 
                 <a href="registerprojects">Register Projects</a>
+                <a href="logout">Logout</a>
             </div>
         </div>
     </div>
