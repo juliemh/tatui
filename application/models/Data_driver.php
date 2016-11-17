9fdfd4eaf269dd0d171107c6e4d93fa7617b326c
s3211063 <s3211063@student.rmit.edu.au>
2016-11-17 21:43:03 +1100

    Add files via upload

---
+++ application/models/Data_driver.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Data_driver extends CI_Model {  
+
+   public function __construct()
+        {
+                // Call the CI_Model constructor
+                parent::__construct();
+                $this->load->database();
+                
+        }
+//
+// get a list of tables
+//
+   public function get_tables() {
+        $table_list = $this->db->list_tables();
+        return $table_list;
+   }
+// 
+// load data from comma delimited file
+//
+public function load_data($tablename, $data) {
+if ($sql = "LOAD DATA INFILE '".$data."' IGNORE INTO TABLE ".$tablename." FIELDS TERMINATED BY ','")
+   {
+   $this->db->query($sql);
+   return TRUE;
+   }
+   else
+   {
+   return FALSE;
+   }
+}   
+//
+// return column names
+//
+public function get_column_count($tablename) {
+         if($this->db->table_exists($tablename)) {
+                  return $this->db->list_fields($tablename);
+                  }
+                  else {
+                  return FALSE;
+            }
+ }
+        
+}
