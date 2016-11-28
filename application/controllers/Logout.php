385b7b2f9fa98eea90749c07f54e1a3ae66859dd
s3211063 <s3211063@student.rmit.edu.au>
2016-11-28 13:21:30 +1100

    redirect to login page

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -12,6 +12,6 @@ class Logout extends CI_Controller
 public function index() {
    $this->session->unset_userdata('user');
    $this->session->sess_destroy();
-   $this->load->view('pages/logout');
+   redirect('pages');
    }
 }
