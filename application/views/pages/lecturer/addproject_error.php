e67c61a0f25fcf06e2839b375c171f2d00431d8a
juliemh <juliemh@optusnet.com.au>
2016-11-30 20:09:23 +1100

    Add files via upload

---
+++ application/views/pages/lecturer/addproject_error.php
@@ -0,0 +1,17 @@
+<div id="page-wrapper">
+
+      
+    <div class="formcontainer">
+        <h2>Uh oh!  Please check there is data in the Course and Subject tables. </h2>
+        <form method="post" action="./addsubject/validate" id="addsubject">
+            <div class="login-container">
+
+               
+        </form>
+
+ <div class='login-username-container'>
+                 <a href="pages" class="btn logoutbtn">Back to Dashboard</a>
+            </div>
+       
+    </div>
+</div>
