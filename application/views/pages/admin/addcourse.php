df7d8d09e3e6a3039d08ca3db6a0ad026b0c131d
juliemh <juliemh@optusnet.com.au>
2016-12-04 16:38:08 +1100

    Update addcourse.php

--- application/views/pages/admin/addcourse.php
+++ application/views/pages/admin/addcourse.php
@@ -1,28 +1,4 @@
-    <div class="formcontainer">
-        <h2>Add Course details here...</h2>
-        <form method="post" action="./addcourse/validate" id="addcourse">
-            <div class="login-container">
-
-                <div class='login-username-container'>
-                    <label>Course ID</label>
-                    <input autofocus  type='text' name="courseid" required>
-                </div>
-                <div class='login-username-container'>
-                    <label>Course Name</label>
-                    <input  type='text' name="coursename" size=100 required>
-                </div>
-                <div class='login-username-container'>
-                    <label>Description</label>
-                    <input  type='text' name="description" size=100 required>
-                </div>
-                <div class='login-username-container'>
-                    <a href="pages" class="btn logoutbtn">Back to Dashboard</a>
-                    <button name="submit" class="btn logoutbtn">  Submit  </button>                  
-                </div>
-            </div>
-        </form>
-    </div>
-    <div class="message-container">
+ <div class="message-container">
          <?php
         $msg1 = $this->session->flashdata('msg1');
         if ((isset($msg1)) && (!empty($msg1))) {
@@ -46,7 +22,32 @@
         ?>
 
 
+    </div>    
+<div class="formcontainer">
+        <h2>Add Course details here...</h2>
+        <form method="post" action="./addcourse/validate" id="addcourse">
+            <div class="login-container">
+
+                <div class='login-username-container'>
+                    <label>Course ID</label>
+                    <input autofocus  type='text' name="courseid" required>
+                </div>
+                <div class='login-username-container'>
+                    <label>Course Name</label>
+                    <input  type='text' name="coursename" size=100 required>
+                </div>
+                <div class='login-username-container'>
+                    <label>Description</label>
+                    <input  type='text' name="description" size=100 required>
+                </div>
+                <div class='login-username-container'>
+                    <a href="pages" class="btn logoutbtn">Back to Dashboard</a>
+                    <button name="submit" class="btn logoutbtn">  Submit  </button>                  
+                </div>
+            </div>
+        </form>
     </div>
+   
 
 
 
